702a442324e2178654f46a94a283a3c522694b09
kedpak <kedpak@yahoo.com>
2017-08-19 01:02:28 +0000

    fix puppet-lint warnings

--- 0x0F-configuration_management/0-create_a_file.pp
+++ 0x0F-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # make puppet file
 file {'/tmp/holberton':
-  path => '/tmp/holberton',
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
