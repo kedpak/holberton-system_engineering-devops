9bd754723c1e4ac2d3eb2cb336aad8a07193ea4c
kedpak <kedpak@yahoo.com>
2017-09-18 23:22:38 +0000

    add condition if arg is not correct raise error

--- 0x16-api/0-gather_data_from_an_API.py
+++ 0x16-api/0-gather_data_from_an_API.py
@@ -17,13 +17,19 @@ if __name__ == '__main__':
     total_tasks = 0
     task_title = []
     name = r_users['name']
-    for i in r_todos:
-        if i['userId'] is int(sys.argv[1]):
-            total_tasks += 1
-        if i['userId'] is int(sys.argv[1]) and i['completed'] is True:
-            task_count += 1
-            task_title.append(i['title'])
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, task_count, total_tasks))
-    for j in task_title:
-        print("\t{}".format(j))
+
+    if len(sys.argv) > 1:
+        try:
+            for i in r_todos:
+                if i['userId'] is int(sys.argv[1]):
+                    total_tasks += 1
+                if i['userId'] is int(sys.argv[1]) and i['completed'] is True:
+                    task_count += 1
+                    task_title.append(i['title'])
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, task_count, total_tasks))
+            for j in task_title:
+                print("\t{}".format(j))
+        except (ValueError):
+            pass
+
