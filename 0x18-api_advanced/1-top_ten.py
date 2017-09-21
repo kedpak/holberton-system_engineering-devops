9ee87fc00427d8c0bc9c13e890f2ae1f8c043537
kedpak <kedpak@yahoo.com>
2017-09-21 23:07:45 +0000

    fix module order

--- 0x18-api_advanced/1-top_ten.py
+++ 0x18-api_advanced/1-top_ten.py
@@ -4,9 +4,10 @@ Print the titles of the first 10 hot posts
 listed for a given subreddit.
 """
 
+import json
 import requests
 import sys
-import json
+
 headers = {
     "User-Agent": "kpak"
 }
@@ -19,10 +20,9 @@ def top_ten(subreddit):
         print (None)
         return
     content = r.json()
-    print(json.dumps(content, indent=4))
     i = 0
     for data in content["data"]["children"]:
-        if data["data"] is None:
+        if data["data"]["title"] is None:
             return
         print(data["data"]["title"])
         i += 1
