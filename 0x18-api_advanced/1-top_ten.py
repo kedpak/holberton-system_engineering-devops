1a5f43f767f1fba9b11c5b699f8f7e0a157a9d72
kedpak <kedpak@yahoo.com>
2017-09-21 22:26:55 +0000

    first commit

---
+++ 0x18-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Print the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
+
+import requests
+import sys
+import json
+headers = {
+    "User-Agent": "kpak"
+}
+
+
+def top_ten(subreddit):
+    url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
+    r = requests.get(url, headers=headers)
+    if not r.ok:
+        return 0
+    content = r.json()
+    i = 0
+    for data in content["data"]["children"]:
+        print(json.dumps(data["data"]["title"], indent=4))
+        i += 1
+        if i == 10:
+            break
