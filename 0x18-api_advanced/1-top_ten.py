79527a0dccc0ec3b48551b9168b30108f3270dac
kedpak <kedpak@yahoo.com>
2017-09-22 00:04:07 +0000

    replaced bracket notation with get

--- 0x18-api_advanced/1-top_ten.py
+++ 0x18-api_advanced/1-top_ten.py
@@ -4,7 +4,6 @@ Print the titles of the first 10 hot posts
 listed for a given subreddit.
 """
 
-import json
 import requests
 
 headers = {
@@ -16,29 +15,32 @@ def top_ten(subreddit):
     url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
     r = requests.get(url, headers=headers)
     if not r.ok:
-        print ('None')
+        print ("None")
         return
     content = r.json()
     i = 0
     if content is None:
-        print ('None')
+        print ("None")
         return
     if content["data"] is None:
-        print ('None')
+        print ("None")
         return
     if content["data"]["children"] is None:
-        print ('None')
+        print ("None")
         return
     for data in content["data"]["children"]:
         if data is None:
-            print ('None')
+            print ("None")
             return
-        if data["data"]["title"] is None:
-            print('None')
+        title = (data.get('data', {}).get('title'))
+        if title is None:
             return
-        print(data["data"]["title"])
+        print(title)
         i += 1
         if i == 10:
             break
     if i == 0:
-        print ('None')
+        print ("None")
+
+if __name__ == "__main__":
+    top_ten(subreddit)
