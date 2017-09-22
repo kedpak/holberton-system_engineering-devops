0ae81fd766ab43eb16c2d4cfbf6bc1f4193f2b77
kedpak <kedpak@yahoo.com>
2017-09-22 03:13:51 +0000

    first commit

---
+++ 0x18-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Print the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
+import requests
+import sys
+
+headers = {
+    "User-Agent": "kpak"
+}
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ print all hot topics in subreddit"""
+    if len(hot_list) == 0:
+        url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
+    else:
+        url = "http://www.reddit.com/r/{}/hot.json?after={}".format(
+            subreddit, after)
+    r = requests.get(url, headers=headers)
+    if not r.ok:
+        print ("None")
+        return
+    content = r.json()
+    hot_post = content["data"]["children"]
+    for i in range(len(content["data"]["children"])):
+        hot_list.append(content["data"]["children"][i]["data"]["title"])
+    if content["data"]["after"] is not None:
+        recurse(subreddit, hot_list, content["data"]["after"])
+    return (hot_list)
